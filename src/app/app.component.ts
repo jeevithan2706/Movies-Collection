ca20c1a03423b873cb1ad9572fd6dbb78cc61982
jeevithan <pxjeevithan@gmail.com>
2023-06-14 09:51:03 +0530

    Commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,105 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'searchmovie';
+
+  searchQuery!: string;
+  movies: any[] = [];
+  selectedFilter: string = 'recent';
+  isDropdownOpen = false;
+
+  languageValues: any = {};
+  regionValues: any = {};
+  releaseType: any = {};
+  category: any = {};
+
+  currentPage: number = 1;
+  totalPages: number = 0;
+
+  ngOnInit() {
+    // this.searchMovies();
+  }
+
+  searchMovies() {
+    const apiKey = '08687b8d6f2e8c8cb41a4d6adf8ba026';
+    let url = `https://api.themoviedb.org/3/${this.searchQuery ? 'search' : 'discover'}/movie?api_key=${apiKey}&query=${this.searchQuery}&page=${this.currentPage}`
+    const languageFilters = Object.keys(this.languageValues).filter(key => this.languageValues[key]);
+    const regionFilters = Object.keys(this.regionValues).filter(key => this.regionValues[key]);
+    const releaseTypeFilter = Object.keys(this.releaseType).filter(key => this.releaseType[key]);
+    const categoryFilter = Object.keys(this.category).filter(key => this.category[key]);
+    const currentDate = new Date();
+
+    // If search query is provided, add the appropriate parameter based on the input
+    url += this.searchQuery
+      ? parseInt(this.searchQuery) ? `${url}&year=${this.searchQuery}` : url
+      : ``;
+
+    //Language filter
+    url += languageFilters.length > 0 ? `&${languageFilters.map(filter => `with_original_language=${filter}`).join('&')}` : '';
+
+    //Region filter
+    url += regionFilters.length > 0 ? `&${regionFilters.map(filter => `region=${filter}`).join('&')}` : '';
+
+    //Releasetype filter
+    url += releaseTypeFilter.length > 0 ? `&with_release_type=${releaseTypeFilter.join(',')}` : '';
+
+    //Category filter
+    url += categoryFilter.length > 0 ? `&with_genres=${categoryFilter.join(',')}` : '';
+
+    // Add recent movies filter
+    if (this.selectedFilter === 'recent') {
+      const oneMonthAgo = new Date();
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+      url += `&primary_release_date.gte=${oneMonthAgo.toISOString().slice(0, 10)}&primary_release_date.lte=${currentDate.toISOString().slice(0, 10)}`;
+    }
+    // Add top movies filter
+    else if (this.selectedFilter === 'top') {
+      url += '&sort_by=popularity.desc';
+    }
+    // Add trending today filter
+    else if (this.selectedFilter === 'today') {
+      const currentDate = new Date().toISOString().slice(0, 10);
+      url += `&primary_release_date.gte=${currentDate}&primary_release_date.lte=${currentDate}`;
+    }
+    // Add trending this week filter 
+    else if (this.selectedFilter === 'week') {
+      const oneWeekAgo = new Date();
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      url += `&primary_release_date.gte=${oneWeekAgo.toISOString().slice(0, 10)}&primary_release_date.lte=${currentDate.toISOString().slice(0, 10)}`;
+    }
+
+    axios.get(url)
+      .then(response => {
+        this.movies = response.data.results;
+        this.totalPages = response.data.total_pages;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  loadPage(page: number) {
+    this.currentPage = page;
+    this.searchMovies();
+  }
+
+  getPaginationArray() {
+    return Array.from({ length: this.totalPages }, (_, index) => index + 1);
+  }
+
+  //To store image url
+  getImageUrl(posterPath: string): string {
+    return `https://image.tmdb.org/t/p/w500/${posterPath}`;
+  }
+
+  //Store the filter data
+  selectFilter(filter: string) {
+    this.selectedFilter = filter;
+    this.searchMovies();
+  }
+
+  //Rating calculator
+  getStars(rating: number): number[] {
+    const roundedRating = Math.round(rating / 2);
+    return Array(roundedRating).fill(0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
